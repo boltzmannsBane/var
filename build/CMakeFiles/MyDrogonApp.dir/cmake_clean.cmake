3add4f272035af00d61aacd6b34d889701821ec0
amygdala <leonapkhazava@gmail.com>
2023-08-15 01:47:21 +0400

    rip bozo

---
+++ build/CMakeFiles/MyDrogonApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyDrogonApp.dir/main.cc.o"
+  "CMakeFiles/MyDrogonApp.dir/main.cc.o.d"
+  "MyDrogonApp"
+  "MyDrogonApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyDrogonApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
