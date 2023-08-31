d35432bc1d0d1e93f87c91ffecf1305c00c8b433
amygdala <leonapkhazava@gmail.com>
2023-09-01 01:26:12 +0400

    swift init executable

--- swift/Package.swift
+++ swift/Package.swift
@@ -5,19 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "swift",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "swift",
-            targets: ["swift"]),
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "swift"),
-        .testTarget(
-            name: "swiftTests",
-            dependencies: ["swift"]),
+        .executableTarget(
+            name: "swift",
+            path: "Sources"),
     ]
 )
