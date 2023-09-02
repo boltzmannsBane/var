b27a8cde2c7f85d4020316009a7d1eb2348e2c7d
amygdala <leonapkhazava@gmail.com>
2023-09-02 22:21:19 +0200

    rip bozo

--- swift/Package.swift
+++ swift/Package.swift
@@ -1,19 +1,23 @@
-// swift-tools-version: 5.8
-
 import PackageDescription
 
 let package = Package(
-    name: "swift",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "swift",
-            path: "Sources"),
-    ],
+    name: "http2-server",
     dependencies: [
-        .product(name: "NIOCore", package: "NIO"),
-        .product(name: "NIOPosix", package: "NIO"),
-        .product(name: "NIOHTTP1", package: "NIO")
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.42.0"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.6.0"),
+        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.9.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .executableTarget(
+            name: "http2-server",
+            dependencies: [
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "NIOSSL", package: "swift-nio-ssl"),
+                .product(name: "NIOHTTP1", package: "swift-nio"),
+                .product(name: "NIOHTTP2", package: "swift-nio-http2"),
+            ]),
     ]
 )
