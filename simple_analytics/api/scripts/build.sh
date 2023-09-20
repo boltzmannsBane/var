8e04e4970c226a98c3b421d03028cbcbef371d66
amygdala <leonapkhazava@gmail.com>
2023-09-20 16:43:21 +0400

    tests

--- simple_analytics/api/scripts/build.sh
+++ simple_analytics/api/scripts/build.sh
@@ -2,4 +2,4 @@ gcc server.c -o server -levent -levent_core -levent_extra
 
 # g++ -o test test.cpp -lgtest -lgtest_main -pthread
 
-g++ -o test test.cpp -lgtest -lgtest_main -pthread -levent
+g++ -o test test.cpp -lgtest -lgtest_main -lgmock -pthread -levent
