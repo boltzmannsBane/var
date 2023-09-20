19ed04b759417df7128da8405971f3875f21b3ae
amygdala <leonapkhazava@gmail.com>
2023-09-20 22:24:05 +0400

    deps management

--- simple_analytics/api/scripts/build.sh
+++ simple_analytics/api/scripts/build.sh
@@ -1,3 +1,3 @@
-gcc server.c -o server -I./deps/libevent/include/ -L./deps/libevent/.libs/ -levent -levent_core -levent_extra
+gcc -I./deps/libevent/include/ -L./deps/libevent/.libs/ -levent -levent_core -levent_extra -o server server.c
 
 # g++ -o test test.cpp -I/deps/gtest/include/ -L/deps/gtest/lib/ -lgtest -lgtest_main -lgmock -pthread -levent -I/deps/libevent/include/ -L/deps/libevent/lib/
