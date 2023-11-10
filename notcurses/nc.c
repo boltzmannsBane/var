2090b1afef7c5b81bbabefe1ba6ec5e9731bcf25
amygdala <leonapkhazava@gmail.com>
2023-11-10 14:03:38 +0400

    in the arena trying stuff

--- notcurses/nc.c
+++ notcurses/nc.c
@@ -1,68 +1,47 @@
-#include <notcurses/notcurses.h>
-#include <unistd.h> // for usleep()
-
-#define BOX_CHAR "▄" // This is a Unicode block character
+#include <stdlib.h>
+#include <notcurses/direct.h>
 
 int main(void) {
-    struct notcurses_options options = {
-        .flags = NCOPTION_NO_ALTERNATE_SCREEN | NCOPTION_SUPPRESS_BANNERS,
-    };
-    struct notcurses* nc = notcurses_core_init(&options, NULL);
-    if(nc == NULL) {
-        return -1;
-    }
+  const char blue[] = "house";
+  const char *b = blue;
 
-    struct ncplane* stdplane = notcurses_stdplane(nc);
+ struct ncdirect* n = ncdirect_core_init(NULL, stdout, 0);
+  int c, ret = 0;
 
-    // Get the dimensions of the plane
-    unsigned rows, cols;
-    ncplane_dim_yx(stdplane, &rows, &cols);
+  // printf("1\n");
+  //
+  // while ((c = getchar()) != EOF) {
+  //     putchar(c);  // Print the character
+  // }
+  //
+  // printf("2\n");
+  //
+  // return 0;
 
-    // Create a new plane for the box with a size of 3x6 characters
-    struct ncplane_options nopts = {
-        .y = 1,
-        .x = 1,
-        .rows = 3,
-        .cols = 6,
-        .userptr = NULL,
-        .name = "box",
-        .resizecb = NULL,
-        .flags = 0
-    };
-    struct ncplane* boxplane = ncplane_create(stdplane, &nopts);
-    if(boxplane == NULL) {
-        notcurses_stop(nc);
-        return -1;
-    }
-
-    // Set the box's background color to blue and foreground to white
-    uint64_t channels = 0;
-    ncchannels_set_fg_rgb(&channels, 0xffffff);
-    ncchannels_set_bg_rgb(&channels, 0x0000ff);
-    ncplane_set_base(boxplane, BOX_CHAR, 0, channels);
-
-    for(unsigned y = 1; y < rows - 3; ++y) {
-        for(unsigned x = 1; x < cols - 6; ++x) {
-            // Move the boxplane's origin
-            ncplane_move_yx(boxplane, y, x);
-
-            // Render the scene
-            if(notcurses_render(nc)) {
-                notcurses_stop(nc);
-                return -1;
-            }
-
-            // Sleep for a bit to slow down the movement
-            usleep(10000); // 50ms
+  if (n) {
+    while (!ret && (c = getchar()) != EOF) {
+      if (isalpha(c) && tolower(c) == *b) {
+        ++b;
+      } else {
+        if (b > blue) {
+          if (!*b) {
+            ret |= ncdirect_on_styles(n, NCSTYLE_BOLD);
+            ret |= ncdirect_set_fg_rgb(n, 0x0339dc);
+          }
+          ret |= (printf("%.*s", (int)(b - blue), blue) < 0);
+          if (!*b) {
+              ret |= ncdirect_set_fg_default(n);
+              ret |= ncdirect_off_styles(n, NCSTYLE_BOLD);
+          }
+          b = blue;
         }
-    }
+        ret |= (putchar(c) == EOF);
+      }
+    }  
+    if (b > blue) {
+        ret |= (printf("%.*s", (int)(b - blue), blue) < 0);
+    } 
+  }
 
-    // Wait for user input
-    notcurses_get_blocking(nc, NULL);
-
-    // Clean up
-    ncplane_destroy(boxplane);
-    notcurses_stop(nc);
-
-    return 0;
+  return (!n || ncdirect_stop(n) || !feof(stdin) || ret) ? EXIT_FAILURE : EXIT_SUCCESS;
 }
